5dec1a78b3c554cd741d9765e57f21f93fb41f13
s550 <deviltrigger87@gmail.com>
2019-06-29 13:10:27 -0700

    Updates

--- precache-manifest.b332475b9c2bafa0540671a74369b164.js
+++ precache-manifest.b332475b9c2bafa0540671a74369b164.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/weather-app-rebirth/static/js/runtime~main.39b4af2f.js"
   },
   {
-    "revision": "8a951e3754b030c8a6fb",
-    "url": "/weather-app-rebirth/static/js/main.8a951e37.chunk.js"
+    "revision": "bf57c9eb72b27c150150",
+    "url": "/weather-app-rebirth/static/js/main.bf57c9eb.chunk.js"
   },
   {
     "revision": "1be2631b363ee3be4676",
     "url": "/weather-app-rebirth/static/js/1.1be2631b.chunk.js"
   },
   {
-    "revision": "8a951e3754b030c8a6fb",
+    "revision": "bf57c9eb72b27c150150",
     "url": "/weather-app-rebirth/static/css/main.ba5d1268.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/weather-app-rebirth/static/css/1.6ca5f82a.chunk.css"
   },
   {
-    "revision": "ec40b5eef36255b7e82733d0f0e505ce",
+    "revision": "a885937fd627b01d3aca564056affccb",
     "url": "/weather-app-rebirth/index.html"
   }
 ];
