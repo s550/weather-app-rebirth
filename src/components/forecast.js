428ff6d4a3d584809f17e10d1d5afee3cb88eee4
s550 <deviltrigger87@gmail.com>
2021-07-30 12:30:53 -0700

    fixed issue with corsanywhere by using a different route.

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -7,9 +7,9 @@ import { Link } from 'react-router-dom';
 
 const Key = "3b84024871aef976d646f09db58974da";
 class Forecast extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       data: [],
       currently: [],
       weekly: [],
@@ -19,78 +19,78 @@ class Forecast extends Component {
     }
 
   }
-  componentDidMount(){
+  componentDidMount() {
     this.weatherReport();
   }
-  weatherReport(lat,lon){
-   if(navigator.geolocation){
-     navigator.geolocation.getCurrentPosition((position) => {
-      axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/3b84024871aef976d646f09db58974da/' + position.coords.latitude + ',' + position.coords.longitude)
-      .then((response) =>{
-        console.log(response)
-        this.setState({dTemp: response.data.currently.temperature, data:response.data, currently:response.data.currently,weekly:response.data.daily.data,forcast:response.data.daily.summary}, function(){
-          console.log(this.state);
-        });
+  weatherReport(lat, lon) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        axios.get('https://secret-ocean-49799.herokuapp.com/https://api.darksky.net/forecast/3b84024871aef976d646f09db58974da/' + position.coords.latitude + ',' + position.coords.longitude)
+          .then((response) => {
+            console.log(response)
+            this.setState({ dTemp: response.data.currently.temperature, data: response.data, currently: response.data.currently, weekly: response.data.daily.data, forcast: response.data.daily.summary }, function () {
+              console.log(this.state);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
-      .catch((err) =>{
-        console.log(err);
-      });
-     })
 
-   }
+    }
 
   }
 
-  showTemp(ans){
-    this.setState({dTemp: ans}, this.weatherReport);
+  showTemp(ans) {
+    this.setState({ dTemp: ans }, this.weatherReport);
   }
-  render(){ 
-        const trueTemp = Math.round(this.state.currently.temperature);
-        const date = new Date().toLocaleDateString();
-        return(
-            <div className="columns">
-            {/* <div className="parent"> */}
-   
-            <div className="column main-top">
-            <div className="card card-logic">
-              <header className="card-header">
-                <h1 className="card-header-title">Right Now: </h1>
-              </header>
-              <div className="card-content">
-                  <div className="content">
-                  <i className={`center-main wi wi-forecast-io-${ this.state.currently.icon}`}></i>
+  render() {
+    const trueTemp = Math.round(this.state.currently.temperature);
+    const date = new Date().toLocaleDateString();
+    return (
+      <div className="columns">
+        {/* <div className="parent"> */}
+
+        <div className="column main-top">
+          <div className="card card-logic">
+            <header className="card-header">
+              <h1 className="card-header-title">Right Now: </h1>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <i className={`center-main wi wi-forecast-io-${this.state.currently.icon}`}></i>
                 <h1 className="daily">{trueTemp}&#xb0;</h1>
-                </div>
-                <div className="content">
+              </div>
+              <div className="content">
                 <h3 className="">{this.state.data.timezone}</h3>
                 <h4><span className="">Current Conditions: </span>{this.state.currently.summary}</h4>
-                <h4><span>Humidity: </span>{this.state.currently.humidity * 100}%</h4>
+                <h4><span>Humidity: </span>{Math.round(this.state.currently.humidity * 100)}%</h4>
                 <h4><span>Dew Point: </span>{Math.round(this.state.currently.dewPoint)}&#xb0;</h4>
 
-                </div>
+              </div>
             </div>
             {/* <h1>Right Now: </h1>
             <div className="daily-top"><i className={`center-main wi wi-forecast-io-${ this.state.currently.icon}`}></i>
             <h1 className="daily">{trueTemp}&#xb0;</h1>
             </div>
-  
+
                 <h3 className="card-title">{this.state.data.timezone}</h3>
                 <h4><span className="desc">Current Conditions: </span>{this.state.currently.summary}</h4>
                 <h4><span>Humidity: </span>{this.state.currently.humidity * 100}%</h4>
                 <h4><span>Dew Point: </span>{Math.round(this.state.currently.dewPoint)}&#xb0;</h4>
                 <h4 className="adj"><Link className="bttn" to="/days">Weekly Forcast</Link></h4> */}
-               <footer className="card-footer">
-                <h4 className="adj card-footer-item"><Link className="bttn" to="/days">Weekly Forcast</Link></h4>
-                 </footer> 
-            
-            </div>
-            </div>
-            
-            {/* </div> */}
-            </div>
-        );
-    }
+            <footer className="card-footer">
+              <h4 className="adj card-footer-item"><Link className="bttn" to="/days">Weekly Forcast</Link></h4>
+            </footer>
+
+          </div>
+        </div>
+
+        {/* </div> */}
+      </div>
+    );
   }
+}
 
 
 export default Forecast;
--- src/components/moment.js
+++ src/components/moment.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Days from './days';
 import Forecast from './forecast';
 // switch board containing routes for home and the weekly forcast.
 
 const SwitchBoard = () => (
     <main>
-        <Switch>
-            <Route exact path="/" component={Forecast}/>
-            <Route path="/days" component={Days}/>
-        </Switch>
-    </main>
+        <BrowserRouter basename={process.env.PUBLIC_URL} >
+            <Switch>
+                <Route exact path="/" component={Forecast} />
+                <Route path="/days" component={Days} />
+            </Switch>
+        </BrowserRouter>
+    </main >
 )
 
 
