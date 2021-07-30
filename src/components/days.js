9189b01663714f2d8676579f53100407ea52f63d
s550 <deviltrigger87@gmail.com>
2021-07-30 14:30:33 -0700

    added in cors workaround for days component.

--- src/components/days.js
+++ src/components/days.js
@@ -14,7 +14,7 @@ class Days extends Component {
         dTemp: [],
         wTemp: []
       }
-  
+
     }
     componentDidMount(){
       this.weatherReport();
@@ -22,7 +22,7 @@ class Days extends Component {
     weatherReport(lat,lon){
      if(navigator.geolocation){
        navigator.geolocation.getCurrentPosition((position) => {
-        axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/3b84024871aef976d646f09db58974da/' + position.coords.latitude + ',' + position.coords.longitude)
+        axios.get('https://secret-ocean-49799.herokuapp.com/https://api.darksky.net/forecast/3b84024871aef976d646f09db58974da/' + position.coords.latitude + ',' + position.coords.longitude)
         .then((response) =>{
           console.log(response)
           this.setState({dTemp: response.data.currently.temperature, data:response.data, currently:response.data.currently,weekly:response.data.daily.data,forcast:response.data.daily.summary}, function(){
@@ -33,19 +33,19 @@ class Days extends Component {
           console.log(err);
         });
        })
-  
+
      }
-  
+
     }
-  
+
     showTemp(ans){
       this.setState({dTemp: ans}, this.weatherReport);
     }
-    render(){ 
+    render(){
           const trueTemp = Math.round(this.state.currently.temperature);
           let days = moment(this.state.weekly.time).fromNow();
           const outlook = this.state.weekly.map(day => (
-              
+
                 <div key={day.time} className="top">
                 <div className="card card-weekly">
                 <header className="card-header">
@@ -56,7 +56,7 @@ class Days extends Component {
                   <i className={`mini-top wi wi-forecast-io-${day.icon}`}></i>
                   </div>
                   <div className="content">
-                  <p className="sum">{day.summary}</p> 
+                  <p className="sum">{day.summary}</p>
                   </div>
                 </div>
                 <footer className="card-footer">
@@ -65,30 +65,30 @@ class Days extends Component {
                 </footer>
                     {/* <h1 className="day">{moment.unix(day.time).format('ddd')}</h1>
                     <i className={`mini-top wi wi-forecast-io-${day.icon}`}></i>
-                       <p className="sum">{day.summary}</p> 
-                   
+                       <p className="sum">{day.summary}</p>
+
                         <p className="desc">High: {Math.round(day.temperatureHigh)}&#xb0;</p>
                         <p className="desc">Low: {Math.round(day.temperatureLow)}&#xb0;</p> */}
-                        
+
                     </div>
                  </div>
-                
+
           ));
           return(
               <div className="container">
               <h1 className="return"><Link className="return" to="/">Return to Daily View</Link></h1>
               <div className="parent">
                 {outlook}
-                
+
               </div>
-              
-              
+
+
               </div>
           );
       }
     }
-  
-  
-  
+
+
+
 
 export default Days;
